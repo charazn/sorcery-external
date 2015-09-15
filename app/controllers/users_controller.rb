130f77374f3b0f241f4a96669eea9748c8e282ba
Char <charazn37@gmail.com>
2015-09-15 15:33:51 +0800

    Add before_action require_login and not_authenticated private method in application_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  skip_before_action :require_login, only: [:index, :new, :create]
 
   def index
     @users = User.all
@@ -18,7 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
+      redirect_to users_path, notice: 'User was successfully created.'
     else
       render :new
     end
@@ -34,7 +35,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    redirect_to users_url, notice: 'User was successfully destroyed.'
+    redirect_to users_path, notice: 'User was successfully destroyed.'
   end
 
   private
