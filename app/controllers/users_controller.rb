016e339a472f315415ad2d8dafb3e235f682d2c2
JasonZ <jason.zeng@tinkerbox.com.sg>
2015-09-16 13:34:12 +0800

    Add autologin @user in users_controller create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      auto_login(@user)
       redirect_to users_path, notice: 'User was successfully created.'
     else
       render :new
